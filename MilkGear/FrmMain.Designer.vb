820e6d802bc7e35ce352f2661ff0000d6b7be0d0
gdkchan <gab.dark.100@gmail.com>
2015-03-19 22:43:07 -0300

    o hai

---
+++ MilkGear/FrmMain.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits MilkGear.gdkForm
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: O procedimento a seguir é exigido pelo Windows Form Designer
+    'Ele pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.PicPanel = New System.Windows.Forms.Panel()
+        Me.Preview = New System.Windows.Forms.PictureBox()
+        Me.BtnExport = New System.Windows.Forms.Button()
+        Me.BtnImport = New System.Windows.Forms.Button()
+        Me.BtnSave = New System.Windows.Forms.Button()
+        Me.BtnOpen = New System.Windows.Forms.Button()
+        Me.TitleBar.SuspendLayout()
+        Me.PicPanel.SuspendLayout()
+        CType(Me.Preview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LblTitle
+        '
+        Me.LblTitle.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.LblTitle.Location = New System.Drawing.Point(4, 4)
+        Me.LblTitle.Size = New System.Drawing.Size(182, 19)
+        Me.LblTitle.Text = "MilkGear v0.3 - by gdkchan"
+        '
+        'TitleBar
+        '
+        Me.TitleBar.Controls.Add(Me.BtnExport)
+        Me.TitleBar.Controls.Add(Me.BtnImport)
+        Me.TitleBar.Controls.Add(Me.BtnSave)
+        Me.TitleBar.Controls.Add(Me.BtnOpen)
+        Me.TitleBar.Controls.SetChildIndex(Me.LblTitle, 0)
+        Me.TitleBar.Controls.SetChildIndex(Me.BtnOpen, 0)
+        Me.TitleBar.Controls.SetChildIndex(Me.BtnSave, 0)
+        Me.TitleBar.Controls.SetChildIndex(Me.BtnImport, 0)
+        Me.TitleBar.Controls.SetChildIndex(Me.BtnExport, 0)
+        '
+        'PicPanel
+        '
+        Me.PicPanel.AutoScroll = True
+        Me.PicPanel.BackColor = System.Drawing.Color.Transparent
+        Me.PicPanel.Controls.Add(Me.Preview)
+        Me.PicPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PicPanel.Location = New System.Drawing.Point(0, 28)
+        Me.PicPanel.Name = "PicPanel"
+        Me.PicPanel.Size = New System.Drawing.Size(640, 356)
+        Me.PicPanel.TabIndex = 17
+        '
+        'Preview
+        '
+        Me.Preview.BackColor = System.Drawing.SystemColors.Control
+        Me.Preview.BackgroundImage = CType(resources.GetObject("Preview.BackgroundImage"), System.Drawing.Image)
+        Me.Preview.Location = New System.Drawing.Point(0, 0)
+        Me.Preview.Name = "Preview"
+        Me.Preview.Size = New System.Drawing.Size(64, 64)
+        Me.Preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.Preview.TabIndex = 18
+        Me.Preview.TabStop = False
+        '
+        'BtnExport
+        '
+        Me.BtnExport.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnExport.Location = New System.Drawing.Point(456, 2)
+        Me.BtnExport.Name = "BtnExport"
+        Me.BtnExport.Size = New System.Drawing.Size(64, 24)
+        Me.BtnExport.TabIndex = 19
+        Me.BtnExport.Text = "&Exportar"
+        Me.BtnExport.UseVisualStyleBackColor = True
+        '
+        'BtnImport
+        '
+        Me.BtnImport.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnImport.Location = New System.Drawing.Point(526, 2)
+        Me.BtnImport.Name = "BtnImport"
+        Me.BtnImport.Size = New System.Drawing.Size(64, 24)
+        Me.BtnImport.TabIndex = 20
+        Me.BtnImport.Text = "&Importar"
+        Me.BtnImport.UseVisualStyleBackColor = True
+        '
+        'BtnSave
+        '
+        Me.BtnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnSave.Location = New System.Drawing.Point(402, 2)
+        Me.BtnSave.Name = "BtnSave"
+        Me.BtnSave.Size = New System.Drawing.Size(48, 24)
+        Me.BtnSave.TabIndex = 18
+        Me.BtnSave.Text = "&Salvar"
+        Me.BtnSave.UseVisualStyleBackColor = True
+        '
+        'BtnOpen
+        '
+        Me.BtnOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnOpen.Location = New System.Drawing.Point(348, 2)
+        Me.BtnOpen.Name = "BtnOpen"
+        Me.BtnOpen.Size = New System.Drawing.Size(48, 24)
+        Me.BtnOpen.TabIndex = 17
+        Me.BtnOpen.Text = "&Abrir"
+        Me.BtnOpen.UseVisualStyleBackColor = True
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.ClientSize = New System.Drawing.Size(640, 384)
+        Me.Controls.Add(Me.PicPanel)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Name = "FrmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Controls.SetChildIndex(Me.TitleBar, 0)
+        Me.Controls.SetChildIndex(Me.PicPanel, 0)
+        Me.TitleBar.ResumeLayout(False)
+        Me.TitleBar.PerformLayout()
+        Me.PicPanel.ResumeLayout(False)
+        Me.PicPanel.PerformLayout()
+        CType(Me.Preview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PicPanel As System.Windows.Forms.Panel
+    Friend WithEvents Preview As System.Windows.Forms.PictureBox
+    Friend WithEvents BtnExport As System.Windows.Forms.Button
+    Friend WithEvents BtnImport As System.Windows.Forms.Button
+    Friend WithEvents BtnSave As System.Windows.Forms.Button
+    Friend WithEvents BtnOpen As System.Windows.Forms.Button
+
+End Class
---
+++ MilkGear/FrmMain.vb
@@ -0,0 +1,191 @@
+﻿Imports System.IO
+Public Class FrmMain
+    Dim Offset As Integer
+    Dim Opened_File() As Byte
+    Dim File_Name As String
+    Dim Has_Open_File As Boolean
+
+    Dim Plain_Method As Boolean
+    Private Sub FrmMain_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.M Then 'Maximiza
+            Me.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size
+            If Me.WindowState = FormWindowState.Maximized Then Me.WindowState = FormWindowState.Normal Else Me.WindowState = FormWindowState.Maximized
+        End If
+
+        If Has_Open_File Then
+            Dim sName As String = Path.GetFileName(File_Name)
+            Dim Input_Files() As FileInfo = New DirectoryInfo(Path.GetDirectoryName(File_Name)).GetFiles()
+            Select Case e.KeyCode
+                Case Keys.Left
+                    For Index As Integer = 1 To Input_Files.Count - 1
+                        If Input_Files(Index).Name = sName Then
+                            Dim i As Integer = 1
+                            Do
+                                Dim CurrFile As String = Input_Files(Index - i).FullName
+                                If Path.GetExtension(CurrFile).ToLower = ".ctxr" Then
+                                    Open(CurrFile)
+                                    Exit Sub
+                                End If
+                                i += 1
+                                If Index - i < 1 Then Exit For
+                            Loop
+                            Exit For
+                        End If
+                    Next
+                Case Keys.Right
+                    For Index As Integer = 0 To Input_Files.Count - 2
+                        If Input_Files(Index).Name = sName Then
+                            Dim i As Integer = 1
+                            Do
+                                Dim CurrFile As String = Input_Files(Index + i).FullName
+                                If Path.GetExtension(CurrFile).ToLower = ".ctxr" Then
+                                    Open(CurrFile)
+                                    Exit Sub
+                                End If
+                                i += 1
+                                If Index + i > Input_Files.Count - 2 Then Exit Sub
+                            Loop
+                            Exit For
+                        End If
+                    Next
+            End Select
+        End If
+    End Sub
+
+    Private Sub BtnOpen_Click(sender As Object, e As EventArgs) Handles BtnOpen.Click
+        Dim OpenDlg As New OpenFileDialog
+        OpenDlg.Filter = "Textura do MGS3|*.ctxr"
+        If OpenDlg.ShowDialog = DialogResult.OK AndAlso File.Exists(OpenDlg.FileName) Then
+            Open(OpenDlg.FileName)
+            PicPanel.Focus()
+        End If
+    End Sub
+    Private Sub Open(FileName As String)
+        Has_Open_File = True
+        File_Name = FileName
+        Opened_File = File.ReadAllBytes(FileName)
+        Dim Width As Integer = (Opened_File(&H2C) * &H100) Or Opened_File(&H2D)
+        Dim Height As Integer = (Opened_File(&H2E) * &H100) Or Opened_File(&H2F)
+
+        Plain_Method = (IsPowerOfTwo(Width) = False) Or (IsPowerOfTwo(Height) = False)
+        Dim Bmp As New Bitmap(Width, Height)
+        Offset = &H80
+        If Plain_Method Then
+            For Y As Integer = 0 To Height - 1
+                For X As Integer = 0 To Width - 1
+                    Bmp.SetPixel(X, Y, Color.FromArgb(Opened_File(Offset), _
+                                                      Opened_File(Offset + 1), _
+                                                      Opened_File(Offset + 2), _
+                                                      Opened_File(Offset + 3)))
+                    Offset += 4
+                Next
+            Next
+        Else
+            Dim Min_Size As Integer = Math.Min(Width, Height)
+            For Y As Integer = 0 To Height - (Min_Size - 1) Step Min_Size
+                For X As Integer = 0 To Width - (Min_Size - 1) Step Min_Size
+                    Decode_Tile(Min_Size, Min_Size, X, Y, Bmp, Opened_File)
+                Next
+            Next
+        End If
+        Preview.Image = Bmp
+        LblTitle.Text = "MilkGear - " & Path.GetFileNameWithoutExtension(File_Name)
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+        If Not Has_Open_File Then Exit Sub
+
+        Dim Min_Size As Integer = Math.Min(Preview.Image.Width, Preview.Image.Height)
+        Dim Bmp As New Bitmap(Preview.Image)
+        Dim Out(((Preview.Width * Preview.Height) * 4) + &H7F) As Byte
+        Buffer.BlockCopy(Opened_File, 0, Out, 0, &H80)
+        Out(&H2C) = (Preview.Image.Width >> 8) And &HFF
+        Out(&H2D) = Preview.Image.Width And &HFF
+        Out(&H2E) = (Preview.Image.Height >> 8) And &HFF
+        Out(&H2F) = Preview.Image.Height And &HFF
+        Offset = &H80
+        If Plain_Method Then
+            For Y As Integer = 0 To Preview.Image.Height - 1
+                For X As Integer = 0 To Preview.Image.Width - 1
+                    Dim Col As Color = Bmp.GetPixel(X, Y)
+                    Out(Offset) = Col.A
+                    Out(Offset + 1) = Col.R
+                    Out(Offset + 2) = Col.G
+                    Out(Offset + 3) = Col.B
+                    Offset += 4
+                Next
+            Next
+        Else
+            For Y As Integer = 0 To Preview.Image.Height - 1 Step Min_Size
+                For X As Integer = 0 To Preview.Image.Width - 1 Step Min_Size
+                    Encode_Tile(Min_Size, Min_Size, X, Y, Bmp, Out)
+                Next
+            Next
+        End If
+
+        File.WriteAllBytes(File_Name, Out)
+        MessageBox.Show("O arquivo foi alterado com sucesso!", "Feito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub BtnExport_Click(sender As Object, e As EventArgs) Handles BtnExport.Click
+        If Not Has_Open_File Then Exit Sub
+
+        Dim SaveDlg As New SaveFileDialog
+        SaveDlg.Filter = "Imagem|*.png"
+        SaveDlg.FileName = Path.GetFileNameWithoutExtension(File_Name)
+        If SaveDlg.ShowDialog = DialogResult.OK Then
+            Preview.Image.Save(SaveDlg.FileName)
+        End If
+    End Sub
+    Private Sub BtnImport_Click(sender As Object, e As EventArgs) Handles BtnImport.Click
+        If Not Has_Open_File Then Exit Sub
+
+        Dim OpenDlg As New OpenFileDialog
+        OpenDlg.Filter = "Imagem|*.png;*.bmp;*.gif;*.jpg;*.tiff"
+        If OpenDlg.ShowDialog = DialogResult.OK AndAlso File.Exists(OpenDlg.FileName) Then
+            Dim Img As New Bitmap(OpenDlg.FileName)
+            Preview.Image = Img
+        End If
+    End Sub
+
+    Private Sub Decode_Tile(Total_Size As Integer, Tile_Size As Integer, PX As Integer, PY As Integer, Bmp As Bitmap, Data() As Byte)
+        If Tile_Size = 0 Then
+            Bmp.SetPixel(PX, PY, Color.FromArgb(Data(Offset), Data(Offset + 1), Data(Offset + 2), Data(Offset + 3)))
+            Offset += 4
+        Else
+            Dim Y As Integer
+            While Y < Total_Size
+                Dim X As Integer = 0
+                While X < Total_Size
+                    Decode_Tile(Tile_Size, Tile_Size \ 2, X + PX, Y + PY, Bmp, Data)
+                    X += Tile_Size
+                End While
+                Y += Tile_Size
+            End While
+        End If
+    End Sub
+    Private Sub Encode_Tile(Total_Size As Integer, Tile_Size As Integer, PX As Integer, PY As Integer, Bmp As Bitmap, Data() As Byte)
+        If Tile_Size = 0 Then
+            Dim Col As Color = Bmp.GetPixel(PX, PY)
+            Data(Offset) = Col.A
+            Data(Offset + 1) = Col.R
+            Data(Offset + 2) = Col.G
+            Data(Offset + 3) = Col.B
+            Offset += 4
+        Else
+            Dim Y As Integer
+            While Y < Total_Size
+                Dim X As Integer = 0
+                While X < Total_Size
+                    Encode_Tile(Tile_Size, Tile_Size \ 2, X + PX, Y + PY, Bmp, Data)
+                    X += Tile_Size
+                End While
+                Y += Tile_Size
+            End While
+        End If
+    End Sub
+
+    Private Function IsPowerOfTwo(Input As ULong) As Boolean
+        Return (Input <> 0) AndAlso ((Input And (Input - 1)) = 0)
+    End Function
+End Class
